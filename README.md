38bc14c46f13ae11b7b1b7c9e319b19bb8340bdd
Dan <dferris@pop-os.localdomain>
2023-09-29 22:51:19 -0700

    security checks

--- README.md
+++ README.md
@@ -39,8 +39,12 @@ Usage of ./gows:
         Path to private key file for TLS (optional)
   -log string
         Log file path (empty for stdout)
+  -password string
+        Password for basic auth (optional)
   -port int
         Port to listen on (default 8889)
+  -username string
+        Username for basic auth (optional)
 ```
 
 ## Example
@@ -64,11 +68,29 @@ openssl req -new -x509 -key server.key -out server.crt -days 365
 
 <answer the questions needed for the CSR>
 
-/gows --cacert server.crt --key server.key --port 8443
+./gows --cacert server.crt --key server.key --port 8443
+
+```
+
+* Use http basic auth
+```
+./gows --username admin --password admin 
+```
+
+* Use http basic auth with TLS
+```
+./gows --cacert server.crt --key server.key --username admin --password admin
 ```
 
 ## Notes
 
 * I have not tested TLS with an intermediary certificate chain at all.
 * TLS is locked to a minimum of version 1.2.  I really don't recommend changing this.
-* Go's http.Dir is not security conscious at all.  It will follow sym links out of your directory tree as well as serving every file in whatever dir you give it.
+* gows will not follow symlinks outside of the directory tree.
+* gows can be set to not follow hard links either.
+* gows will not allow access to dot files
+* gows will not traverse directories
+
+There are very good security reasons to not traverse directories, follow links, and avoid dot files.  Don't put private information onto the public Internet.  I'm not responsible if you manage to leak data. 
+
+This is not a standard web server.  If you want to serve static content for a web site, use a real web server like nginx.  This is a simple server that will allow you to easily share some files if need be.
--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
+	"syscall"
 	"time"
 )
 
@@ -67,6 +70,76 @@ func configureTLS(certFile, keyFile, chainFile string) (*tls.Config, error) {
 	return tlsConfig, nil
 }
 
+func checkLink(dir string, file string, checkHardLinks bool) error {
+	fullPath := filepath.Join(dir, file)
+	fileInfo, err := os.Lstat(fullPath)
+	if err != nil {
+		return err
+	}
+
+	if fileInfo.Mode()&os.ModeSymlink != 0 {
+		linkDest, err := os.Readlink(fullPath)
+		if err != nil {
+			return err
+		}
+
+		abspath, err := filepath.Abs(filepath.Dir(filepath.Join(dir, linkDest)))
+		if err != nil {
+			return err
+		}
+
+		if abspath != filepath.Clean(dir) {
+			return fmt.Errorf("insecure symlink: %s", file)
+		}
+	}
+
+	fileinfo, err := os.Stat(fullPath)
+	if err != nil {
+		return err
+	}
+
+	if checkHardLinks {
+		if stat, ok := fileinfo.Sys().(*syscall.Stat_t); ok {
+			nlink := stat.Nlink
+			if nlink > 1 {
+				return fmt.Errorf("hardlink detected: %s", file)
+			}
+		}
+	}
+
+	return nil
+}
+
+func isDotFile(dir string, file string) error {
+	path := filepath.Join(dir, file)
+	for _, part := range strings.Split(path, string(os.PathSeparator)) {
+			if strings.HasPrefix(part, ".") {
+					return fmt.Errorf("dot file detected: %s", file)
+			}
+	}
+	return nil
+}
+
+func isInAnotherDir(dir string, file string) error {
+	finfo, _ := os.Stat(filepath.Join(dir, file))	
+	
+	if finfo.IsDir() {
+		return fmt.Errorf("directory detected: %s", file)
+	}
+
+	return nil
+}
+
+func checkBasicAuth(r *http.Request, expectedUsername string, expectedPassword string) bool {
+	username, password, ok := r.BasicAuth()
+	if ok {
+		if username == expectedUsername && password == expectedPassword {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	port := flag.Int("port", 8889, "Port to listen on")
 	ListenIP := flag.String("ip", "127.0.0.1", "IP to listen on")
@@ -75,6 +148,9 @@ func main() {
 	certFile := flag.String("cacert", "", "Path to CA certificate file for TLS (optional)")
 	keyFile := flag.String("key", "", "Path to private key file for TLS (optional)")
 	chainFile := flag.String("chain", "", "Path to intermediate certificate chain file for TLS (optional)")
+	username := flag.String("username", "", "Username for basic auth (optional)")
+	password := flag.String("password", "", "Password for basic auth (optional)")
+	checkHardLinks := flag.Bool("checkhardlinks", false, "Check for hardlinks (optional)")
 
 	flag.Parse()
 
@@ -93,7 +169,7 @@ func main() {
 		logger = log.New(lf, "", 0)
 	}
 
-	dir := *directory
+	dir := filepath.Clean(*directory)
 
 	if dir == "." || dir == "" {
 		cwd, err := os.Getwd()
@@ -107,8 +183,32 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var authStatus bool
+
+		if *username != "" && *password != "" {
+			authStatus = checkBasicAuth(r, *username, *password)
+		} else {
+			authStatus = true
+		}
+
 		rw := &responseWriterWithSize{w, 0, 0}
-		http.FileServer(http.Dir(dir)).ServeHTTP(rw, r)
+
+		if !authStatus {
+			rw.Header().Set("WWW-Authenticate", `Basic realm="Enter username and password"`)
+			http.Error(rw, "401 unauthorized", http.StatusUnauthorized)
+			return
+		} else {
+			path := filepath.Clean(r.URL.Path)
+			if path != "/" {
+				if checkLink(dir, path, *checkHardLinks) != nil || 
+				   isDotFile(dir, path) != nil || 
+				   isInAnotherDir(dir, path) != nil {
+						http.Error(rw, "403 forbidden", http.StatusForbidden)
+						return
+				}
+			} 
+			http.FileServer(http.Dir(dir)).ServeHTTP(rw, r)
+		}
 		logRequest(logger, r, rw.Size, rw.StatusCode)
 	})
 
